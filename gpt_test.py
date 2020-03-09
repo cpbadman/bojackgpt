1111d009ec7faad4cdc10b2319198806622f4026
Ciaran Badman <cpbadman@gmail.com>
2020-03-09 19:08:14 +0000

    did a new run

--- gpt_test.py
+++ gpt_test.py
@@ -4,18 +4,18 @@ import requests
 
 model_name = "124M"
 if not os.path.isdir(os.path.join("models", model_name)):
-	print(f"Downloading {model_name} model...")
-	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
+    print(f"Downloading {model_name} model...")
+    gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
 
 
-file_name = "allEpiosdesRaw.txt"
+file_name = "allEpisodesRaw.txt"
 if not os.path.isfile(file_name):
-	url = "https://raw.githubusercontent.com/cpbadman/bojackgpt/master/allEpisodesRaw.txt" 
-	data = requests.get(url)
-	
-        with open(file_name, 'w') as f:
-		f.write(data.text)
-    
+    url = "https://raw.githubusercontent.com/cpbadman/bojackgpt/master/allEpisodesRaw.txt" 
+    data = requests.get(url)
+
+    with open(file_name, 'w') as f:
+        f.write(data.text)
+
 
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
